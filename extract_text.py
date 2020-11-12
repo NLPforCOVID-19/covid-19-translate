19ad203b69dca183610c2313acbe1c33b01fb7b8
shyyhs <songskg6666@gmail.com>
2020-11-12 15:54:00 +0900

    extract_text

--- extract_text.py
+++ extract_text.py
@@ -9,6 +9,7 @@ import glob
 import random
 from pathlib import Path
 from datetime import datetime, timedelta
+from utils import get_base_tag, check_list_page, get_html_file
 
 from bs4 import BeautifulSoup
 import html2text
@@ -17,7 +18,7 @@ import requests as req
 from requests_oauthlib import OAuth1
 from xml.etree.ElementTree import *
 
-source_langs = ["jp", "cn", "es", "eu", "us", "int", "de", "fr", "kr", 'in']
+source_langs = ["jp", "cn", "br", "kr", "es", "eu", "us", "int", 'in', "de", "fr", "au", 'za', 'np', 'my']
 source_langs_ori = source_langs
 keywords = ["COVID", "covid", "肺炎", "コロナ", "corona", "Corona", "코로나"] # En, zh, ja, fr, ko
 
@@ -178,6 +179,12 @@ def read_all_input_htmls_with_lang_and_site_limit(parent_folder, lang, limit_num
                         old_names.append((line, lang))
     return all_names
 
+def line_link_page(html_file):
+    soup = get_html_file(html_file)
+    base_tag, base_text = get_base_tag(soup)
+    res = check_list_page(base_tag)
+    return res
+
 
 def gene_tmp(name, lang):
     tmp_name = name.replace("/{}/orig/".format(lang), "/{}/tmp/".format(lang))
@@ -189,9 +196,19 @@ def gene_tmp(name, lang):
     title_main_name = str(Path(tmp_name).with_suffix(".title_main"))
     meta_name = str(Path(tmp_name).with_suffix(".meta"))
     #abs_name = turn_to_abs(name, /mnt/hinoki/share/covid19)
-    with open(extract_accessed_file_list, "a+") as f:
-        f.write(name.strip()+'\n')
+
+    ##fix
+    #result, content_num, link_num = extract_source_text_xml(block_name)
+    #percent = float(link_num+1)/float(link_num+content_num+1)
+    #if (percent>0.66 and percent<0.8):
+    #    print ("test percent")
+    #    print (name)
+    #    print (percent)
+    #return
+
     if (os.path.exists(meta_name) == 1):
+        with open(extract_accessed_file_list, "a+") as f:
+            f.write(name.strip()+'\n')
         return
 
     if (lang=='jp'):
@@ -219,16 +236,29 @@ def gene_tmp(name, lang):
         timestamp = os.path.getmtime(name)
         lang = lang
         keyword_flag = contain_keyword(result)
+
+        #link_page_flag = line_link_page(name)
+        #print (name)
+        #print (link_page_flag)
+        #print (content_num, link_num)
+        #if (link_page_flag==True):
+        #    content_num=1
+        #    link_num=100
         meta_info = "{} {} {} {} {} {}".format(translated_flag, timestamp, lang, content_num, link_num, keyword_flag)
 
         with open(meta_name, "w") as f:
             f.write(meta_info)
         tot_num+=1
+        with open(extract_accessed_file_list, "a+") as f:
+            f.write(name.strip()+'\n')
+
         #print ("New trans {} of lang {}".format(tot_num, lang))
         #print ("block generated: {}".format(block_name))
         #print ("title_main generated: {}".format(title_main_name))
         #print ("meta generated: {}".format(meta_name))
     except:
+        with open(extract_accessed_file_list, "a+") as f:
+            f.write(name.strip()+'\n')
         return
 
 def write_to_access(names):
@@ -245,13 +275,17 @@ while (1):
     #print ("# in each lang: {}".format(now_num))
     for lang in source_langs:
         all_names = read_all_input_htmls_with_lang_and_site_limit(parent_folder, lang, limit_num)
-
+        #fix
         before_filter = len(all_names)
         all_names = [name for name in all_names if (extract_accessed_files.get(name[0], 0)==0)]
         after_filter = len(all_names)
 
+        if (after_filter == 0):
+            time.sleep(10)
+            continue
         print ("Got files")
         print (after_filter)
+
 
         #if (after_filter > limitation):
         #    #all_names.sort(key = lambda x: priority(x))
